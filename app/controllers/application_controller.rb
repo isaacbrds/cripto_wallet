f6714a34ed244ce09360a5476115e091e0f860a4
ibrds <ibrds@ig.com.br>
2018-12-03 13:30:02 -0300

    bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    before_action :set_locale
+
+    private        
+    def set_locale
+        if params[:locale]
+            session[:locale] =  params[:locale] 
+            I18n.locale = session[:locale]
+        end
+    end
 end
